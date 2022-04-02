39df91be192d3421ec4b64f0601dd88681574694
Ciceron <100491130+MarcusTuliusCiceron@users.noreply.github.com>
2022-04-02 22:37:22 +0200

    late night update

--- app/js/app.js
+++ app/js/app.js
@@ -171,10 +171,25 @@ class BackingDetails extends React.Component {
     }
 }
 
+class BackingItem extends React.Component{
+    render(){
+        return <div className="Backing_Item">
+            
+        </div>
+    }
+}
+
 class BackingMenu extends React.Component{
     render(){
         return <div className="Backing_Menu">
-            
+            <h2>About this project</h2>
+            <p>The Mastercraft Bamboo Monitor Riser is a sturdy and stylish platform that elevates your screen 
+  to a more comfortable viewing height. Placing your monitor at eye level has the potential to improve 
+  your posture and make you more comfortable while at work, helping you stay focused on the task at hand.</p>
+        <p>
+        <br/>Featuring artisan craftsmanship, the simplicity of design creates extra desk space below your computer 
+        to allow notepads, pens, and USB sticks to be stored under the stand.</p>
+        <BackingItem></BackingItem>
         </div>
     }
 }
@@ -203,25 +218,9 @@ ReactDOM.render(<Page/>, document.querySelector('#app'))
 /*
 
 
-
-  
   
 
   
-  
-
-  
-  
-  
-
-  About this project
-
-  The Mastercraft Bamboo Monitor Riser is a sturdy and stylish platform that elevates your screen 
-  to a more comfortable viewing height. Placing your monitor at eye level has the potential to improve 
-  your posture and make you more comfortable while at work, helping you stay focused on the task at hand.
-
-  Featuring artisan craftsmanship, the simplicity of design creates extra desk space below your computer 
-  to allow notepads, pens, and USB sticks to be stored under the stand.
   
   Bamboo Stand
   Pledge $25 or more
